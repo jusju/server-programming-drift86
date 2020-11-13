00430d413c52d2e4a8489514b8a4bf0eea6fd728
Temelius <teme989@gmail.com>
2020-11-13 20:30:52 +0200

    Initial project commit

---
+++ src/main/java/com/temelius/drift86/Drift86Application.java
@@ -0,0 +1,13 @@
+package com.temelius.drift86;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Drift86Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Drift86Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/temelius/drift86/Drift86ScoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temelius.drift86;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Drift86ScoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
