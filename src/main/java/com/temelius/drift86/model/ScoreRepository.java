b632bc481c065dc967f320dea71a7217e1c5c151
Temelius <teme989@gmail.com>
2020-11-19 08:12:16 +0200

    Add findAllByVerified method

--- src/main/java/com/temelius/drift86/model/ScoreRepository.java
+++ src/main/java/com/temelius/drift86/model/ScoreRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface ScoreRepository extends CrudRepository<Score, Long> {
 	List<Score> findByUser(String user);
 	List<Score> findAllByMap(Map map);
+	List<Score> findAllByVerified(boolean isVerified);
 }
