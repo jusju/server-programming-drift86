e897b79aecf7af49d93537c3cf3c4b8fa708fbe9
Temelius <teme989@gmail.com>
2020-11-19 08:11:03 +0200

    Add logoutSuccessUrl

--- src/main/java/com/temelius/drift86/WebSecurityConfig.java
+++ src/main/java/com/temelius/drift86/WebSecurityConfig.java
@@ -34,6 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
           .permitAll()
           .and()
       .logout()
+      	  .logoutSuccessUrl("/")
           .permitAll();
 	}
 	
