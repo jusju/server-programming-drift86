47261f858f2b773941bb4f7da8beb3b0026e33b4
Temelius <teme989@gmail.com>
2020-11-19 08:13:16 +0200

    Add findAllByOrderByRoleAsc

--- src/main/java/com/temelius/drift86/model/UserRepository.java
+++ src/main/java/com/temelius/drift86/model/UserRepository.java
@@ -1,7 +1,10 @@
 package com.temelius.drift86.model;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User,Long> {
 	User findByUsername(String username);
+	List<User> findAllByOrderByRoleAsc();
 	}
